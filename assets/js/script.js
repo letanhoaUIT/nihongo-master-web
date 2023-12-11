37d1d3d5249dec98c332634da01bb36591d6a650
Tan Hoa <letanhoa2003@gmail.com>
2023-12-11 10:54:02 +0700

    create header

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length  > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+/**
+ * navbar toggle 
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+
+}
+
+ addEventOnElem(navToggler, "click", toggleNavbar);
